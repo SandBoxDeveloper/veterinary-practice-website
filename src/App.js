1aa1a85b5eb1b12da6e20b7d82fb79702f09103d
andreh-sanuk <85935416+andreh-sanuk@users.noreply.github.com>
2021-11-17 20:47:22 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
 
 function App() {
+
+  const [petList, setPetList] = useState([])
+
+  useEffect(() => {
+    fetch("http://0.0.0.0:8080/pets")
+    .then(response => response.json())
+    .then(data => setPetList(data ))
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <b>List of Pets</b>
+      {petList.map((pet)=> <div>{ `Pet name is: ${pet.name} with id: ${pet.id}` }</div>)}
     </div>
   );
 }
